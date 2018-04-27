7a0728e68f8ee5e42b6b49288fa13aaedab55a4c
Nguyen Tan Dat <ntdatjoker@gmail.com>
2018-04-27 11:20:33 +0700

    Update readme and move box btn to center (#2)
    * Add core button and font awesome
    
    * Add MessageBox component
    
    * Finish message box chat #1
    
    * Finish message box chat #2
    
    * Update readme and move box btn to center

--- src/App.js
+++ src/App.js
@@ -12,21 +12,25 @@ export default class App extends PureComponent {
 
   renderToggleButton() {
     return (
-      <Button
-        outline
-        color="primary"
-        className="mt-auto mr-3 p-0 rounded-circle"
-        style={{ width: '60px', height: '60px' }}
-        onClick={this.toggleModal}
-      >
-        <i className="fa fa-lg fa-comments" />
-      </Button>
+      <section className="m-auto d-flex flex-column align-items-center justify-content-center">
+        <h5>Simple Message Box</h5>
+        <Button
+          outline
+          id="targetButton"
+          color="primary"
+          className="mr-3 p-0 rounded-circle"
+          style={{ width: '60px', height: '60px' }}
+          onClick={this.toggleModal}
+        >
+          <i className="fa fa-lg fa-comments" />
+        </Button>
+      </section>
     )
   }
 
   render() {
     return (
-      <section className="w-100 h-100 p-3 d-flex justify-content-end">
+      <section className="w-100 h-100 p-3 d-flex">
         {this.renderToggleButton()}
         <MessageBox isOpen={this.state.isOpen} toggleModal={this.toggleModal}/>
       </section>
